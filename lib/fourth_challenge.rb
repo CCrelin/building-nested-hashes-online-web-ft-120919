9d15bbeb8f3351e32c1a18b3cf5ebbb5962e2ab4
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-01 23:26:17 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         name: "", age: "", attitude: ""
-        
+        name: "", age: "", attitude: ""
       ]
    }, 
    :capulet => {
@@ -15,6 +15,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         name: "", age: "", attitude: ""
+        
       ]
    }
 }
